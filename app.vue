69188202fd6a91d0f7e95044e8f6d2a7e8477c93
leo@maczedong <leonard.huth@protonmail.com>
2025-07-16 21:19:07 +0200

    initialize project with Nuxt, TypeScript, and ESLint configuration

---
+++ app.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <UTable :columns="columns" :data="data"/>
+  </div>
+</template>
+<script setup lang="ts">
+  const {data} = await useFetch('http://127.0.0.1:8000/therapeuts')
+  const columns = [
+    {
+      accessorKey: 'name',
+      header: 'Name'
+    },
+    {
+      accessorKey: 'email',
+      header: 'Email'
+    },
+    {
+      accessorKey: 'contacted_date',
+      header: 'Contacted On'
+    },
+    {
+      accessorKey: 'response_received',
+      header: 'Response Received'
+    },
+    {
+      accessorKey: 'answer_status',
+      header: 'Answer'
+    },
+  ]
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true
+    }
+  },
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/ui-pro'
+  ],
+  css: ['~/assets/css/main.css']
+})
